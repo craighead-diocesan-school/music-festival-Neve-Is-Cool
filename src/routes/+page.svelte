c6919c33c600993c8e028ba07c268fd51da7b7f7
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-05-19 14:01:46 +1200

    Nav Component Complete
    Navigation component completed, without induvidualised name details as these remain same throughout site. CSS completed, fully functional and aesthetically pleasing in both chrome and microsoft edge browsers. Pages of site added for navigation to function.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,25 +1,24 @@
 <script>
   import Header from "$lib/Header.svelte"
-  import BulmaExamples from "$lib/BulmaExamples.svelte"
+  import Nav from "$lib/Nav.svelte"
 </script>
 
 <Header heading="Sunburst Festival" subheading="General details" />
+<Nav />
 
 <main class="content section">
   <h2>SvelteKit</h2>
 
   <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
-
-  <p>This template comes loaded with the <a href="https://bulma.io/documentation/">Bulma CSS framework</a>, so you can save time and focus on your project.</p>
-
-  <p>Here's some examples of colour helpers. You can change the colours in <code>theme.scss</code></p>
-  <BulmaExamples />
 </main>
-
 <footer class="footer">
   <p class="has-text-centered">&copy; Craighead Diocesan School 2025</p>
 </footer>
 
 <style>
   @import url("https://fonts.googleapis.com/css2?family=Belanosima:wght@400;600;700&family=Sniglet:wght@400;800&display=swap");
+
+  main {
+    background-color: #fbf1b2;
+  }
 </style>
