1fc279a420a9559d812f06caff26c26ea5046f61
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-05-22 08:45:58 +1200

    Comments added
    Some comments added regarding next steps.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -14,7 +14,7 @@
     <li>Festival goers must be 15 years of age or older</li>
   </ul>
   <p>Welcome to the ultimate music festival experience! Get ready for a day filled with live music, great food, and unforgettable memories.</p>
-  <!--Add image here -- need to decide what actually doing-->
+  <!--Add image here. Will make a component for this. Could use a figcaption for each image as well within the image component-->
   <p>Our festival features a diverse lineup of talented musicians from different genres, sure to satisfy every taste in music. With multiple stages set up throughout the venue, you can easily move from one act to the next, discovering new favorites and enjoying classic hits.</p>
   <p>From up-and-coming artists to established headliners, we have curated an exciting lineup that will keep you dancing and singing along all day long. Our festival is the perfect opportunity to discover new music, see your favorite artists live, and make lasting memories with friends and family.</p>
   <p>But the music is just the beginning. Our festival also boasts an impressive selection of food vendors, offering everything from classic festival fare to more gourmet options. You won't go hungry with the delicious treats and refreshing beverages available throughout the venue.</p>
