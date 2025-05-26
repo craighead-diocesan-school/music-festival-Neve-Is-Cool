ee1fa993cb3456fdfbfd4f47e877528f7775cfe2
Neve-Is-Cool <145308823+Neve-Is-Cool@users.noreply.github.com>
2025-05-26 13:21:10 +1200

    Content Added
    Content added to accommodation page

--- src/routes/accommodation/+page.svelte
+++ src/routes/accommodation/+page.svelte
@@ -9,5 +9,12 @@
 <Nav />
 <main>
   <Image src="krists-luhaers.jpg" alt="Festival goers in tents" caption="Krists Luhaers" />
+  <p>Experience the ultimate music festival adventure with our tent accommodation packages! With our comfortable and convenient tents, you can immerse yourself in the festival atmosphere and stay close to all the action.</p>
+  <p>Our tent accommodation packages are designed to provide you with a hassle-free and enjoyable stay. Whether you're attending the festival with friends or family, our tents offer a cozy and secure space to relax after a long day of music and festivities.</p>
+  <p>Our tents are spacious and come equipped with everything you need for a comfortable stay. With comfortable beds, pillows, and blankets, you can get a good night's sleep and wake up feeling refreshed and ready for another day of music and fun.</p>
+  <p>Located within the festival grounds, our tents are just a short walk away from all the stages and attractions. You won't have to worry about transportation or navigating crowds to get to the festival – you'll be right in the heart of it all.</p>
+  <p>Our tents also come with a range of amenities to make your stay even more comfortable. You'll have access to clean restroom facilities, hot showers, and charging stations for your devices. We also have a dedicated staff available to assist with any needs or questions you may have during your stay.</p>
+  <p>But the best part of our tent accommodation packages is the community. You'll be surrounded by other festival-goers who are just as excited as you are to be a part of the experience. You'll make new friends, share stories, and create memories that will last a lifetime.</p>
+  <p>Don't miss out on this opportunity to enhance your music festival experience. Book your tent accommodation package today and get ready for an unforgettable adventure. We can't wait to see you there!</p>
 </main>
 <Footer />
